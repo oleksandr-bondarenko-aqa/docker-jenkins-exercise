391d4da6e0b67c633cffc4ae1130400f03274f54
Oleksandr Bondarenko <oleksandr.bondarenko.qa@gmail.com>
2024-09-26 17:53:00 +0300

    add fail test

--- test/loginTest.js
+++ test/loginTest.js
@@ -5,36 +5,42 @@ describe('Login Page Test', function() {
     let browser;
     let page;
 
-    // Increase timeout for Mocha to 10 seconds
     this.timeout(10000);
 
-    // Before hook to launch the browser
     before(async () => {
         browser = await chromium.launch({ headless: true });
         const context = await browser.newContext();
         page = await context.newPage();
     });
 
-    // After hook to close the browser if it was initialized
     after(async function() {
         if (browser) {
             await browser.close();
         }
     });
 
-    it('should log in successfully with valid credentials', async function() {
-        // Step 1: Visit the login page
+    it('Should log in successfully with valid credentials', async function() {
         await page.goto('https://practicetestautomation.com/practice-test-login/');
 
-        // Step 2: Fill in username and password fields
-        await page.fill('#username', 'student'); // Fill the username
-        await page.fill('#password', 'Password123'); // Fill the password
+        await page.fill('#username', 'student');
+        await page.fill('#password', 'Password123');
 
-        // Step 3: Click the submit button
         await page.click('#submit');
 
-        // Step 4: Verify that "Logged In Successfully" message is displayed
-        await page.waitForSelector('.post-title'); // Wait for the element to appear
+        await page.waitForSelector('.post-title');
+        const successMessage = await page.textContent('.post-title');
+        expect(successMessage).to.include('Logged In Successfully');
+    });
+
+    it('Fail test', async function() {
+        await page.goto('https://practicetestautomation.com/practice-test-login/');
+
+        await page.fill('#username', 'studentt');
+        await page.fill('#password', 'Password123');
+
+        await page.click('#submit');
+
+        await page.waitForSelector('.post-title');
         const successMessage = await page.textContent('.post-title');
         expect(successMessage).to.include('Logged In Successfully');
     });
