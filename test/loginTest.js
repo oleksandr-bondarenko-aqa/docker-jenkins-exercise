fcd494dd68bc4a96bc1ecaef50f4bcc1a4822b3e
Oleksandr Bondarenko <oleksandr.bondarenko.qa@gmail.com>
2025-03-05 14:53:16 +0200

    test webhook

--- test/loginTest.js
+++ test/loginTest.js
@@ -43,5 +43,5 @@ describe('Login Page Test', function() {
     //     const errorMessage = await page.textContent('.error');
     //     expect(errorMessage).to.include('Your username is invalid!');
     // });
-    //test
+    //test1
 });
